1f4999174458763e00b598f3f9ea3cd034464144
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-09-30 02:07:45 -0700

    Change the directory layout for the project
    add components and pages directory. Also add router to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
-    <>
-    </>
+    <Router>
+    </Router>
   )
 }
 
