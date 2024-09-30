c6e8e1fb0f96ea7a5d89820f299cb5a16710fa65
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-09-30 15:41:18 -0700

    Add Banner component and waterfall photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import NavBar from '../components/NavBar';
+import Banner from '../components/Banner';
 
 const Home = () => {
   return (
     <>
       <NavBar/>
+      <Banner/>
     </>
   )
 }
