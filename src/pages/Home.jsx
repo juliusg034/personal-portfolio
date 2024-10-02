2e101e5c376e47d1d8309dd2a80cf7ca948554e9
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-10-01 20:04:49 -0700

    add home.css file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import NavBar from '../components/NavBar';
 import Banner from '../components/Banner';
+import '../styles/Home.css';
 
 const Home = () => {
   return (
-    <>
+    <div className="home">
       <NavBar/>
       <Banner/>
-    </>
+    </div>
   )
 }
 
